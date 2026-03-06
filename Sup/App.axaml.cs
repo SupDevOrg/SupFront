c43e011ca66f7343a554b9e002fbb1102bad418b
YuraGolyshev <yura.golyshev2021@bk.ru>
2026-03-06 20:56:37 +0400

    Поменял на актуальную ссылку ApiBaseUrl и WsBaseUrl

--- Sup/App.axaml.cs
+++ Sup/App.axaml.cs
@@ -7,8 +7,8 @@ namespace Sup
     public partial class App : Application
     {
         // ƒÀﬂ ÀŒ ¿À‹Õ€’ “≈—“Œ¬ «¿Ã≈Õ»“‹ ApiBaseUrl Õ¿ "http://localhost:8080/api/v1/"
-        public static string ApiBaseUrl { get; } = "http://85.239.55.179:80/api/v1/";
-        public static string WsBaseUrl { get; } = "ws://85.239.55.179:80/api/v1/message/ws";
+        public static string ApiBaseUrl { get; } = "https://ample-determination-dev.up.railway.app/api/v1/";
+        public static string WsBaseUrl { get; } = "ws://ample-determination-dev.up.railway.app/api/v1/message/ws";
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
